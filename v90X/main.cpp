bb3449a549ad2a2c4550e847e713e598c4cbd954
sidiousvic <victor6rene@gmail.com>
2019-12-21 23:14:17 +0900

    Initial Commit

---
+++ v90X/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  v90X
+//
+//  Created by VIC SIDIOUS on 2019/12/21.
+//  Copyright © 2019 SIDIOUSVIC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
